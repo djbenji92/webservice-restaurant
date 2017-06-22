cd2fdf3469e68449687bf6b6d53107dcdc7930d9
djbenji92 <benjamin.rouville@gmail.com>
2017-06-22 11:13:34 +0200

    update header

--- actions/categories/remove.js
+++ actions/categories/remove.js
@@ -3,6 +3,10 @@ module.exports = (server) => {
     //const Bot = server.models.Bot;
 
     return (req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         Categorie.findByIdAndRemove(req.params.id, (err, cat) => {
             if (err)
                 return res.status(500).send(err);
--- actions/categories/update.js
+++ actions/categories/update.js
@@ -2,6 +2,10 @@ module.exports = (server) => {
     const Categorie = server.models.Categorie;
 
     return (req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         Categorie.findByIdAndUpdate(req.params.id,
             req.body,
             (err, instance) => {
